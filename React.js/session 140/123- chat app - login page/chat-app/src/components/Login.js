ecfdcad0915aa765774bb8b88616b9d0c4baff3a
Mohammad Reza esmailzadeh <smr8012101067@gmail.com>
2022-12-31 16:33:13 +0330

    141 completed

---
+++ React.js/session 140/123- chat app - login page/chat-app/src/components/Login.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+// Icons 
+import google from "../assets/google.svg";
+
+// Styles
+import styles from "./Login.module.css";
+
+const Login = () => {
+    return (
+        <div className={styles.loginPage}>
+            <div className={styles.loginCard}>
+                <h2>Welcome to Botogram!</h2>
+
+                <div className={styles.button}>
+                    <img src={google} alt="google" /> Sign in with Google
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
---
+++ React.js/session 140/chat-app/src/App.js
@@ -0,0 +1,16 @@
+import { Switch, Route } from "react-router-dom";
+
+// Components
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route path="/" component={Login} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
