fad8d9291e11fe1b144965c5e92bf5ea85cb83a3
Mohammad Reza esmailzadeh <smr8012101067@gmail.com>
2023-01-01 16:05:49 +0330

    sa

--- React.js/session 144/126- chat app - logout section/chat-app/src/components/Navbar.js
+++ React.js/session 144/126- chat app - logout section/chat-app/src/components/Navbar.js
@@ -3,11 +3,13 @@ import React from "react";
 // Styles
 import styles from "./Navbar.module.css";
 
-const Navbar = () => {
+const Navbar = ({ logoutHandler }) => {
   return (
     <div className={styles.container}>
       <div className={styles.name}>Matrixgram</div>
-      <div className={styles.logout}>Logout</div>
+      <div className={styles.logout} onClick={logoutHandler}>
+        Logout
+      </div>
     </div>
   );
 };
