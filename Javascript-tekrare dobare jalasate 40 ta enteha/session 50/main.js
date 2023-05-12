b960b3b5c3ec91ac5691db36b0767893595b27dd
Mohammad Reza esmailzadeh <smr8012101067@gmail.com>
2023-05-12 20:16:15 +0430

    added 'todo-list' project to my git repository

--- Javascript-tekrare dobare jalasate 40 ta enteha/session 50/main.js
+++ Javascript-tekrare dobare jalasate 40 ta enteha/session 50/main.js
@@ -3,10 +3,12 @@ const userData = {
   name: "Mamali",
   age: "27",
 };
-localStorage.setItem("userData", JSON.stringify(userData));
+localStorage.setItem("userData", JSON.stringify(userData))
 
+console.log(localStorage.getItem("name"));
 const data = localStorage.getItem("userData");
+console.log("ddd = ", data);
 const parsedData = JSON.parse(data);
 console.log(parsedData);
-localStorage.removeItem("name")
-localStorage.clear()
+// localStorage.removeItem("name")
+// localStorage.clear()
